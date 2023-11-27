ca6a9007bf73533fd2057daff97b60b5098dfc16
MateusTeixeira <mateus.teixeira@icarotech.com>
2023-11-26 21:25:41 -0300

    feat: implementando algoritmo improved bubble sort

---
+++ BubbleSort/improvedBubbleSort.py
@@ -0,0 +1,50 @@
+import time
+import random
+
+def improvedBubbleSort(arr):
+    n = len(arr)
+
+    for i in range(n):
+        swapped = False
+
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+
+        if swapped == False:
+            break
+
+# Caso 1: Melhor Caso - Valores em ordem crescente
+print("\nMelhor Caso: Valores em ordem crescente (já ordenados)")
+arr1 = [5, 6, 7, 8, 9]
+improvedBubbleSort(arr1)
+print("Lista ordenada: ", arr1)
+
+# Caso 2: Caso Médio - Valores desordenados com números aleatórios
+print("\nCaso Médio: Valores desordenados com números aleatórios")
+arr2 = [5, 2, 9, 1, 7]
+improvedBubbleSort(arr2)
+print("Lista ordenada: ", arr2)
+
+# Caso 3: Pior Caso - Valores em ordem decrescente
+print("\nPior Caso: Valores em ordem decrescente")
+arr3 = [9, 8, 7, 6, 5]
+improvedBubbleSort(arr3)
+print("Lista ordenada: ", arr3)
+
+# Teste de desempenho para os três casos e tamanhos de lista
+for n in [1000, 10000, 100000]:
+    for caso in [1, 2, 3]:
+        if caso == 1:
+            arr = [random.randint(1, n) for _ in range(n)]
+            arr.sort()
+        elif caso == 2:
+            arr = [random.randint(1, n) for _ in range(n)]
+        else:
+            arr = [random.randint(1, n) for _ in range(n)]
+            arr = arr[::-1]
+        start_time = time.time()
+        improvedBubbleSort(arr)
+        end_time = time.time()
+        print("\nCaso: ", caso, " | Tamanho da lista: ", n, " | Tempo de execução: ", end_time - start_time, " segundos")
