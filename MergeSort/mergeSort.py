7935f88293b55c2ab6aaccf1df75b6936e6164e2
MateusTeixeira <mateus.teixeira@icarotech.com>
2023-11-26 22:09:14 -0300

    feat: implementando algoritmo merge sort

--- MergeSort/mergeSort.py
+++ MergeSort/mergeSort.py
@@ -1,52 +1,33 @@
 import random
 import time
 
-def mergeSort(arr):
-    if len(arr) > 1:
-        mid = len(arr) // 2
-        left = arr[:mid]
-        right = arr[mid:]
+def quickSort(arr):
+    if len(arr) <= 1:
+        return arr
 
-        mergeSort(left)
-        mergeSort(right)
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
 
-        i = j = k = 0
-
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                arr[k] = left[i]
-                i += 1
-            else:
-                arr[k] = right[j]
-                j += 1
-            k += 1
-
-        while i < len(left):
-            arr[k] = left[i]
-            i += 1
-            k += 1
-
-        while j < len(right):
-            arr[k] = right[j]
-            j += 1
-            k += 1
+    return quickSort(left) + middle + quickSort(right)
 
 # Caso 1: Melhor Caso - Valores em ordem crescente
 print("\nMelhor Caso: Valores em ordem crescente (já ordenados)")
 arr1 = [5, 6, 7, 8, 9]
-mergeSort(arr1)
+arr1 = quickSort(arr1)
 print("Lista ordenada: ", arr1)
 
 # Caso 2: Caso Médio - Valores desordenados com números aleatórios
 print("\nCaso Médio: Valores desordenados com números aleatórios")
 arr2 = [5, 2, 9, 1, 7]
-mergeSort(arr2)
+arr2 = quickSort(arr2)
 print("Lista ordenada: ", arr2)
 
 # Caso 3: Pior Caso - Valores em ordem decrescente
 print("\nPior Caso: Valores em ordem decrescente")
 arr3 = [9, 8, 7, 6, 5]
-mergeSort(arr3)
+arr3 = quickSort(arr3)
 print("Lista ordenada: ", arr3)
 
 # Teste de desempenho para os três casos e tamanhos de lista
@@ -60,7 +41,9 @@ for n in [1000, 10000, 100000]:
         else:
             arr = [random.randint(1, n) for _ in range(n)]
             arr = arr[::-1]
+        
         start_time = time.time()
-        mergeSort(arr)
+        arr = quickSort(arr)
         end_time = time.time()
+        
         print("\nCaso: ", caso, " | Tamanho da lista: ", n, " | Tempo de execução: ", end_time - start_time, " segundos")
